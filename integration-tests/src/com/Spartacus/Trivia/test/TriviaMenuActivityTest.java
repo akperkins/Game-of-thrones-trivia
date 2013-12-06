b4fc92eaae6cebe4131d280f12903ac9afc16474
Andre Perkins <akperkins1@gmail.com>
2013-12-06 08:08:14 -0500

    minor changes

--- integration-tests/src/com/Spartacus/Trivia/test/TriviaMenuActivityTest.java
+++ integration-tests/src/com/Spartacus/Trivia/test/TriviaMenuActivityTest.java
@@ -4,4 +4,20 @@ import android.test.AndroidTestCase;
 
 public class TriviaMenuActivityTest extends AndroidTestCase {
 
+	public TriviaMenuActivityTest() {
+		super();
+	}
+
+	public void setUp() {
+
+	}
+
+	public void tearDown() {
+
+	}
+
+	public void testPreconditions() {
+		//should be used to test that the AUT is set up properly
+	}
+
 }
