b2ca26a551797ef9fa237aedfc4453a0ca9e3979
Andre Perkins <akperkins1@gmail.com>
2014-02-16 23:52:56 -0500

    started working on integrating json parser

---
+++ app/src/com/GameOfThrones/Trivia/data/RequestWebService.java
@@ -0,0 +1,44 @@
+package com.GameOfThrones.Trivia.data;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+
+public class RequestWebService extends AsyncTask<String, Integer, String> {
+	@Override
+	protected String doInBackground(String... uri) {
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpResponse response;
+		String responseString = null;
+		try {
+			response = httpClient.execute(new HttpGet(uri[0]));
+			StatusLine statusLine = response.getStatusLine();
+			if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				response.getEntity().writeTo(out);
+				out.close();
+				responseString = out.toString();
+			} else {
+				// closes the connection
+				response.getEntity().getContent().close();
+				throw new IOException(statusLine.getReasonPhrase());
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseString;
+	}
+
+}
+
